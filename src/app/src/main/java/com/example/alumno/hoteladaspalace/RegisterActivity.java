d39cd59bc7a04fb18be90def258b5495eeb4fe30
JeanPaulChamorro <i1315198@continental.edu.pe>
2018-12-06 21:43:50 -0500

    registroinicio

--- src/app/src/main/java/com/example/alumno/hoteladaspalace/RegisterActivity.java
+++ src/app/src/main/java/com/example/alumno/hoteladaspalace/RegisterActivity.java
@@ -2,13 +2,14 @@ package com.example.alumno.hoteladaspalace;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
+    Button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
-
