cf1a114cdcc12d4596b9f0b8f2d6cf104c946f69
emil <i1620750@continental.edu.pe>
2018-12-21 22:42:41 -0500

    menu

--- src/app/src/main/java/com/example/alumno/hoteladaspalace/Models/Registro.java
+++ src/app/src/main/java/com/example/alumno/hoteladaspalace/Models/Registro.java
@@ -3,6 +3,8 @@ package com.example.alumno.hoteladaspalace.Models;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.alumno.hoteladaspalace.R;
+
 public class Registro extends AppCompatActivity {
 
     @Override
